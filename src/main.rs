7b75e30db984cd2c1281a24eac0dc9a34cca82fb
vivek2584 <vivekroy2584@gmail.com>
2025-06-08 16:54:26 +0000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let cmdline_args: Vec<String> = env::args().collect();
 
-    let pattern = &cmdline_args[1];
-    let file_name = &cmdline_args[2];
+    let input = Input::new(&cmdline_args).unwrap_or_else(|err| {
+        println!("Failed to parse args : {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {} in {}\n", pattern, file_name);
+    println!("Searching for {} in {}\n", input.pattern, input.file_name);
 
-    let contents = fs::read_to_string(file_name).expect("Unable to open specified file path!");
+    if let Err(e) = run(input) {
+        println!("failed to read from file");
+        process::exit(1);
+    }
+}
 
-    println!("{}", contents);
+struct Input {
+    pattern: String,
+    file_name: String,
+}
+
+impl Input {
+    fn new(args: &[String]) -> Result<Input, &str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
+
+        let pattern = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Input {
+            pattern: pattern,
+            file_name: file_name,
+        })
+    }
+}
+
+fn run(input: Input) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(input.file_name)?;
+
+    Ok(())
 }
